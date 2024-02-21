62145a257d5b93c16448ab08a33333b9f061eb21
Andrés Harisa Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2024-02-20 20:10:13 -0800

    added icon

--- src/menu/menu.py
+++ src/menu/menu.py
@@ -1,11 +1,15 @@
 ''' menu core, ties together ui and function layout for the menu '''
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from src.menu.main_window import MainWindow
 
 class MainMenu():
     ''' main menu for the app, core that ties together ui and logic '''
     def __init__(self):
-        self.app = QApplication([]) #creates the application, need one of these per app
+        self.app = QApplication([]) #creates the application
+        self.app.setApplicationName('TRAQ Sensor QA') #sets the application name
+        self.app.setApplicationDisplayName('TRAQ Sensor QA') #sets the application display name
+        self.app.setWindowIcon(QIcon('assets/icon.png')) #sets the application icon
         self.window = MainWindow()
 
     def run(self):
